cfe3648738ed71c6efd699b1936f24432e707e54
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2021-11-19 23:59:25 +0800

    finished 0.71

--- src/javascripts/food_hotels.js
+++ src/javascripts/food_hotels.js
@@ -71,7 +71,7 @@ const onlyFoodAndHotels = (() =>{
         item += `
         <li class="item shadow" data-index="${currentIndex}">
           <div class="image-wrapper">
-            <img src="../images/placeholder.svg" alt="">
+            <img src="https://raw.githubusercontent.com/LunZaiZai0223/THE_F2E_3rd_Week01/main/src/images/placeholder.svg" alt="">
           </div>
           <div class="content">
             <h2 class="title">${HotelName}</h2>
@@ -98,7 +98,8 @@ const onlyFoodAndHotels = (() =>{
     data.forEach((obj, currentIndex) => {
       const { Name, Address } = obj;
       const { PictureUrl1 } = obj.Picture;
-      item += `
+      if (PictureUrl1 !== undefined ) {
+        item += `
       <li class="item shadow" data-index=${currentIndex}>
         <div class="image-wrapper">
           <img src="${PictureUrl1}" alt="">
@@ -108,6 +109,18 @@ const onlyFoodAndHotels = (() =>{
           <p><span><svg style="transform: translateY(3px);" width="16" height="16"><path d="M5.5 13.4444C5.5 13.4444 11 9.77778 11 5.5C11 4.04131 10.4205 2.64236 9.38909 1.61091C8.35764 0.579463 6.95869 0 5.5 0C4.04131 0 2.64236 0.579463 1.61091 1.61091C0.579463 2.64236 0 4.04131 0 5.5C0 9.77778 5.5 13.4444 5.5 13.4444ZM7.33388 5.49991C7.33388 6.51243 6.51307 7.33324 5.50055 7.33324C4.48803 7.33324 3.66721 6.51243 3.66721 5.49991C3.66721 4.48739 4.48803 3.66658 5.50055 3.66658C6.51307 3.66658 7.33388 4.48739 7.33388 5.49991Z" fill="#ff1d6c"></path></svg>${Address}</span></p>
         </div>
         </li>`;
+      } else {
+          item += `
+        <li class="item shadow" data-index=${currentIndex}>
+          <div class="image-wrapper">
+            <img src="https://raw.githubusercontent.com/LunZaiZai0223/THE_F2E_3rd_Week01/main/src/images/placeholder.svg" alt="">
+          </div>
+          <div class="content">
+            <h2 class="title">${Name}</h2>
+            <p><span><svg style="transform: translateY(3px);" width="16" height="16"><path d="M5.5 13.4444C5.5 13.4444 11 9.77778 11 5.5C11 4.04131 10.4205 2.64236 9.38909 1.61091C8.35764 0.579463 6.95869 0 5.5 0C4.04131 0 2.64236 0.579463 1.61091 1.61091C0.579463 2.64236 0 4.04131 0 5.5C0 9.77778 5.5 13.4444 5.5 13.4444ZM7.33388 5.49991C7.33388 6.51243 6.51307 7.33324 5.50055 7.33324C4.48803 7.33324 3.66721 6.51243 3.66721 5.49991C3.66721 4.48739 4.48803 3.66658 5.50055 3.66658C6.51307 3.66658 7.33388 4.48739 7.33388 5.49991Z" fill="#ff1d6c"></path></svg>${Address}</span></p>
+          </div>
+          </li>`;
+      }
     });
     return item;
   }
